4d9f81b536e361cadaefad69a142d82ed14f6fd8
Doãn Văn Vân <doanvanvandvv@gmail.com>
2019-09-15 09:03:05 +0700

    - update

---
+++ socialape-functions/functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream body',
+      createdAt: '2019-09-13T18:02:49.439Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
